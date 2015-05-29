f817c1dc62b13f173eace66cd11a456804247f96
Forbes Lindesay <fobes@lindesay.co.uk>
2015-05-29 13:38:44 +0100

    Update to match readme

--- bin/cli.js
+++ bin/cli.js
@@ -4,7 +4,7 @@
 
 var foreground = require('foreground-child');
 var test = require('../');
-if (process.argv[2] === 'cover') {
+if (process.argv[2] === 'cover' || process.argv[2] === 'coverage') {
   foreground(process.argv[0], [require.resolve('istanbul/lib/cli'), 'cover', __filename]);
 } else {
   test(require(process.cwd()), process.cwd() + '/test');
