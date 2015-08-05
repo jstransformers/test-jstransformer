6dc521605b7541c9b8ac8f2826c5db05f2fe896c
tunnckoCore <mameto_100@mail.bg>
2015-08-06 00:00:55 +0300

    cameCase title names

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var extname = require('path').extname;
 var basename = require('path').basename;
 var resolve = require('path').resolve;
 var testit = require('testit');
+var camelCase = require('camelcase')
 
 function assertEqual(output, expected) {
   try {
@@ -58,6 +59,7 @@ function read(filename) {
 module.exports = addTests;
 function addTests(transform, testDirectory, test) {
   test = test || testit;
+  var titleName = camelCase(transform.name)
 
   function addTestCases(directory) {
     var inputFile = getFilename(join(directory, 'input.*'));
